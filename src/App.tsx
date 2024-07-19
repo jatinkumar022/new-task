3eb11519f18d0f1e61d09dae8081a2b388d25e58
jatinkumar022 <jatinrmn@gmail.com>
2024-07-19 14:42:07 +0530

    new path

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,7 @@ const App = () => {
       <CssVarsProvider theme={darkTheme} defaultMode="dark">
         <Routes>
           <Route path="new-task/" element={<Home />}>
-            <Route index element={<Navigate to="/all-tasks" replace />} />
-            <Route path="all-tasks" element={<AllTasks />} />
+            <Route index path="all-tasks" element={<AllTasks />} />
             <Route path="important" element={<Important />} />
             <Route path="completed" element={<Completed />} />
             <Route path="do-it-now" element={<DoItNow />} />
