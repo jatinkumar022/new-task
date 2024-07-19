5d2b350a110dc27f87a2188232eeb3855c047ded
jatinkumar022 <jatinrmn@gmail.com>
2024-07-19 14:14:38 +0530

    redefined app

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,29 @@
 import {
-  createBrowserRouter,
-  RouterProvider,
+  // BrowserRouter as Router,
   Navigate,
+  Route,
+  Routes,
 } from "react-router-dom";
 import { AllTasks, Completed, DoItNow, Important } from "./pages";
 import Home from "./pages/Home";
 import { CssVarsProvider } from "@mui/joy/styles";
 import darkTheme from "./Theme/ThemeProvider";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Home />,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="/all-tasks" replace />,
-        },
-        {
-          path: "all-tasks",
-          element: <AllTasks />,
-        },
-        {
-          path: "important",
-          element: <Important />,
-        },
-        {
-          path: "completed",
-          element: <Completed />,
-        },
-        {
-          path: "do-it-now",
-          element: <DoItNow />,
-        },
-      ],
-    },
-  ],
-  { basename: "/new-task" }
-);
-
 const App = () => {
   return (
-    <CssVarsProvider theme={darkTheme} defaultMode="dark">
-      <RouterProvider router={router} />
-    </CssVarsProvider>
+    <>
+      <CssVarsProvider theme={darkTheme} defaultMode="dark">
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Navigate to="/all-tasks" replace />} />
+            <Route path="all-tasks" element={<AllTasks />} />
+            <Route path="important" element={<Important />} />
+            <Route path="completed" element={<Completed />} />
+            <Route path="do-it-now" element={<DoItNow />} />
+          </Route>
+        </Routes>
+      </CssVarsProvider>
+    </>
   );
 };
 
